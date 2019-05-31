27cb05314cdf134b8aae3811aed30aa40e1bb594
taurayimuzenda <50917419+taurayimuzenda@users.noreply.github.com>
2019-05-31 16:37:37 +0200

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-int		ft_isalnum(int c)
+#include	"libft.h"
+int		ft_isalnum (int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 0 && c <= 9))
 		return (1);
