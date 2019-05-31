effcf4d4c7dacbeded2b262af0953b03b45f5a94
taurayimuzenda <50917419+taurayimuzenda@users.noreply.github.com>
2019-05-31 15:44:09 +0200

    edited ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include		<stdio.h>
-#include		<string.h>
+#include	<stdio.h>
+#include	<string.h>
 
 
 char				*ft_strnstr(const char *haystack, const char *needle, size_t n)
@@ -21,19 +21,19 @@ char				*ft_strnstr(const char *haystack, const char *needle, size_t n)
 
 	b = needle;
 
-	if(*b == '\0')
-		return(char *)haystack;
-	if(*haystack == '\0')
+	if (*b == '\0')
+		return (char *) haystack;
+	if (*haystack == '\0')
 		return (NULL);
-	while(*haystack != '\0'&& n > 0 )
+	while (*haystack != '\0' && n > 0 )
 	{
-		if(*haystack == *b)
+		if (*haystack == *b)
 		{
 			a = haystack;
-			while(*a == *b || *b == '\0')
+			while (*a == *b || *b == '\0')
 			{
-				if(*b == '\0')
-					return (char *)haystack;
+				if (*b == '\0')
+					return (char *) haystack;
 				a++;
 				b++;
 			}
@@ -41,16 +41,16 @@ char				*ft_strnstr(const char *haystack, const char *needle, size_t n)
 		haystack++;
 		n++;
 	}
-	return(char *)needle;
+	return (char *) needle;
 }
-/*int				main(void)
+/*int				main (void)
 {
 	char *dest = "Hello World Taurayi is here";
 	char *src = "World";
 	char *result;
 	result = strnstr(dest, src, 10);
-	printf("built: %s\n", result);
-	//printf("strstr:  %s\n",strstr(dest, src));
-	printf("mine: %s\n", ft_strnstr(dest, src, 10));
+	printf ("built: %s\n", result);
+	//printf ("strstr:  %s\n",strstr(dest, src));
+	printf ("mine: %s\n", ft_strnstr(dest, src, 10));
 	return (0);
 }*/
